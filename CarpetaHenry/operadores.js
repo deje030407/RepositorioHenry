d415d2d8c3396c2537e6f85bc2a68e86d10fc3bc
deje030407 <47284468+deje030407@users.noreply.github.com>
2023-01-21 22:34:51 -0500

    operadores

---
+++ CarpetaHenry/operadores.js
@@ -0,0 +1,18 @@
+//Suma
+console.log(3+5);
+
+//Multiplicar
+console.log(8*5);
+
+//Modulo o Resto
+console.log(95 % 4);
+
+//Precedencia
+console.log(3 + 5 * 2 - 8);
+
+//Concatenar
+console.log('Hola' + ' Camilo');
+var nombre = ' Jean';
+var saludo = 'HOLA' + nombre;
+console.log(saludo);
+
